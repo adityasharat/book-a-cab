187017a2e71adac7cf74c96ec5c50b11cd1e376b
Aditya Sharat <aditya.sharat@flipkart.com>
2015-04-21 21:05:04 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-(function ($) {}($));
+(function ($) {
+
+    function init() {
+        var $pages = $('.page');
+        var $buttonBookCab = $('#button-book-cab');
+        var $buttonConfirm = $('#button-confirm');
+
+        function goToPage(selector) {
+            return function () {
+                $pages.hide();
+                $pages.find(selector).show();
+            };
+        }
+        $buttonBookCab.on('click', goToPage('plan'));
+        $buttonConfirm.on('click', goToPage('.confirm'));
+    }
+
+    $(document).ready(init);
+
+}($));
