46d0c3aa0d406f955f1d6e53e7341bdbef5e4c04
Aditya Sharat <aditya.sharat@flipkart.com>
2015-05-18 21:36:22 +0530

    updates

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 (function ($) {
 
-    function init() {
-        var $returnTripCheckbox = $('#chkbx-r-trip');
-        var $returnTimeElement = $("#r-time-elem");
+  function init() {
+    var $checkboxReturnTrip = $('#chkbx-r-trip');
+    var $elementReturnTime = $('#r-time-elem');
+    var $buttonFindCabs = $('btn-search');
+    var $elementCabSelection = $('#cab-selection-elem');
 
-        $returnTripCheckbox.on('click', function () {
-            $returnTimeElement.toggle(this.checked);
-            console.log(this.checked);
-        });
-    }
+    $checkboxReturnTrip.on('click', function () {
+      $elementReturnTime.toggle(this.checked);
+      console.log(this.checked);
+    });
 
-    $(document).ready(init);
+    $buttonFindCabs.on('click', function () {
+      $elementCabSelection.show();
+    });
+  }
+
+  $(document).ready(init);
 
 }($));
